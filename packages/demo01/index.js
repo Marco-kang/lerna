f29746d51a04009440e498e8ae5468f0fc772773
kang-kevin <491022087@qq.com>
2020-04-03 16:52:33 +0800

    update

--- packages/demo01/index.js
+++ packages/demo01/index.js
@@ -1,7 +1,6 @@
 const demo02 = require('demo02')
-
+// const demo03 = require('demo03')
 const mode01 = () => {
-  console.log(demo02.getName())
+  console.log(demo02.getName(), demo02.getAge())
 }
 mode01()
-// export default demo01
